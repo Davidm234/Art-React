8134bdca5dd3ef8cabb652d2ee7bae31a943e97a
David Marquez <142251961+Davidm234@users.noreply.github.com>
2024-07-08 21:45:00 -0400

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        <a
+          href="https://github.com/Davidm234/Art-React"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Repository
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
