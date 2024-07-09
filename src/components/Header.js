3a241626758fdfaee9f5e09522e68d564eb30ef0
David Marquez <142251961+Davidm234@users.noreply.github.com>
2024-07-08 21:45:32 -0400

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Art Institute of Chicago</h1>
+      <p1> Type an art id to see an artwork from the collection. </p1>
+    </header>
+  );
+};
+
+export default Header;
